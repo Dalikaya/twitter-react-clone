632d5e5135d13e1c0a0c3d7cb9670dd2403331fb
Dalikaya <dalikaya@gmail.com>
2020-10-19 02:09:15 +0300

    navigation updated

--- components/navigation.js
+++ components/navigation.js
@@ -12,7 +12,7 @@ import {
   Profile,
   More
 } from './icons'
-import TitleBold from './title-bold'
+import TextTitle from './text-title'
 import styles from './navigation.module.css'
 
 function Navigation({selectedKey}) {
@@ -24,43 +24,43 @@ function Navigation({selectedKey}) {
 
       <NavigationButton selected = {selectedKey === 'home'}>
         <Home />
-        <TitleBold>Home</TitleBold>
+        <TextTitle>Home</TextTitle>
     </NavigationButton>
     
 
       <NavigationButton   selected = {selectedKey === 'explore'}>
         <Explore />
-        <TitleBold>Explore</TitleBold>
+        <TextTitle>Explore</TextTitle>
       </NavigationButton>
 
       <NavigationButton notify={17}  selected = {selectedKey === 'notification'}>
         <Notification />
-        <TitleBold>Notification</TitleBold>
+        <TextTitle>Notification</TextTitle>
       </NavigationButton>
 
       <NavigationButton   selected = {selectedKey === 'messages'}>
         <Messages />
-        <TitleBold>Messages</TitleBold>
+        <TextTitle>Messages</TextTitle>
       </NavigationButton>
 
       <NavigationButton   selected = {selectedKey === 'bookmark'}>
         <Bookmark />
-        <TitleBold>Bookmark</TitleBold>
+        <TextTitle>Bookmark</TextTitle>
       </NavigationButton>
 
       <NavigationButton   selected = {selectedKey === 'list'}>
         <Lists />
-        <TitleBold>Lists</TitleBold>
+        <TextTitle>Lists</TextTitle>
       </NavigationButton>
 
       <NavigationButton   selected = {selectedKey === 'profile'}>
         <Profile />
-        <TitleBold>Profile</TitleBold>
+        <TextTitle>Profile</TextTitle>
       </NavigationButton>
 
       <NavigationButton   selected = {selectedKey === 'more'}>
         <More />
-        <TitleBold>More</TitleBold>
+        <TextTitle>More</TextTitle>
       </NavigationButton>
     </nav>
   )
---
+++ components/text-title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import cn from 'classnames'
+import styles from './text-title.module.css'
+
+export default function TextTitle({bold=true, children}) {
+    return (
+        <h2 className={cn(styles.title,bold && styles.bold)}>
+           {children} 
+        </h2>
+    )
+}
