01de56ea49aa5205ebcf86749d6e009798b22590
Dalikaya <dalikaya@gmail.com>
2020-10-19 11:49:59 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Button from '../components/button'
 
+import Navigation from '../components/navigation';
+
 function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
-      <Button>Merhaba</Button>
+      <Navigation></Navigation>
     </div>
   )
 }
