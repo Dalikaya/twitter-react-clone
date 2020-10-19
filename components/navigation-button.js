9b2c90b5f8bdd3b851ac6e621afc0f82ad995726
Dalikaya <dalikaya@gmail.com>
2020-10-19 11:49:48 +0300

    new

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -6,6 +6,7 @@ import Button from './button'
 import styles from './navigation-button.module.css'
 
 function NavigationButton({ notify,selected, children,...props }) {
+
   return <Button className={cn(styles.navButton,selected && styles.navButtonSelected)} {...props}>{children}
   
 {notify &&  <span className={styles.notify}>{notify}</span>}
